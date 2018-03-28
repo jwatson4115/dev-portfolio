40c9db0392344427e45ea0a0cc12340e60e4dc82
James Watson <jwatson4115@outlook.com>
2018-03-28 03:03:33 +0100

    Break down page into separate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { LeadComponent } from './lead/lead.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ContactComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    EducationComponent,
+    ExperienceComponent,
+    HeaderComponent,
+    AboutComponent,
+    LeadComponent
   ],
   imports: [
     BrowserModule
